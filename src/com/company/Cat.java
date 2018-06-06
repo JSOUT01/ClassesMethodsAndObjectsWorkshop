84f8573da72a37a76c336220691c9798e1fa19fc
James Southerland <james.f.southerland@gmail.com>
2018-06-06 14:14:25 -0400

    Created Dog and Pet Classes

--- src/com/company/Cat.java
+++ src/com/company/Cat.java
@@ -1,27 +1,37 @@
 package com.company;
 
-public class Cat {
+public class Cat extends Pet {
 
-    String name;
+//    String name;
     boolean wiskers;
     boolean fur;
     int legs;
     double weight;
     String furColor;
 
-    public Cat(String name, int legs, double weight) {
-        this.name = name;
-        this.legs = legs;
+//    public Cat(String ownerName, String petName, int age, char gender, String homeAdress) {
+//        super(ownerName, petName, age, gender, homeAdress);
+//    }
+
+    public Cat(String ownerName, String petName, int age, char gender, String homeAdress, boolean fur, double weight) {
+        super(ownerName, petName, age, gender, homeAdress);
+        this.fur = fur;
         this.weight = weight;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    //    public Cat(String name, int legs, double weight) {
+//        this.name = name;
+//        this.legs = legs;
+//        this.weight = weight;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 
     public boolean isWiskers() {
         return wiskers;
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+Cat myCat = new Cat("Crystal","Abby", 3,'f',"Pikeville",true,10);
 
+Cat jamesCat = new Cat("James","Maris",4,'f',"4621 Beaver Road",true, 11.5);
+
+System.out.println("The name of my cat is: " + jamesCat.getPetName());
 
     }
 }
